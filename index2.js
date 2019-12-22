3bc426831548ca0ac81d3eda3345304a0c9e9786
peyo <peteryyoon@gmail.com>
2019-12-22 15:22:01 -0800

    added state info to index2.js

--- index2.js
+++ index2.js
@@ -1,8 +1,8 @@
 "use strict";
 
-const NpsApiKey = "Add Key";
+const NpsApiKey = "9WM6xhhATZ9TowDYuhGGFiB5nRGPLcRtSKCbc9MZ";
 const NpsSearchURL = "https://developer.nps.gov/api/v1/parks";
-const GoogleApiKey = "Add Key";
+const GoogleApiKey = "AIzaSyDWl7qyVdWeTZeOUmsVpBXM9rQcS9kFbRw";
 const GoogleGeocodeURL = "https://maps.googleapis.com/maps/api/geocode/json";
 
 function formatQueryParams(params) {
@@ -17,7 +17,7 @@ function displayResults(responseJson) {
      $("#results-list").empty();
      for (let i = 0; i < responseJson.data.length; i++){
          $("#results-list").append(
-         `<li><h3>${responseJson.data[i].fullName}</h3>
+        `<li><h3>${responseJson.data[i].fullName}, ${responseJson.data[i].states}</h3>
          <p>${responseJson.data[i].designation}</p>
          <p>${responseJson.data[i].description}</p>
          <p><a href="${responseJson.data[i].url}">${responseJson.data[i].url}</a></p>
@@ -121,11 +121,14 @@ function addAddress(merged) {
 
 function watchForm() {
     $("form").submit(event => {
-        event.preventDefault();
-        const state = $("#js-state-code").val();
-        const limit = $("#js-max-results").val();
-        getParkDetails(state, limit);
+      event.preventDefault();
+      const state = $("#js-state-searches").val();
+      const limit = $("#js-max-results").val();
+      
+      let newState = state;
+      newState = state.replace(/\s/g, "");
+      getParkDetails(newState, limit);
     });
-}
+  }
 
 $(watchForm);
